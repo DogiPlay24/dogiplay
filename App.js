9109cda8aa943bc0b222cbef6a217e064ad0d9d7
CarlosAmezcua <Amezcua041196@gmail.com>
2024-08-28 18:45:01 -0700

    ADD: archivos de fuentes

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [load, error] = useFonts({
+    DancingSc: require("./assets/fonts/DancingSc.ttf"),
+    "DancingSc-Medium": require("./assets/fonts/DancingSc-Medium.ttf"),
+    "DancingSc-Bold": require("./assets/fonts/DancingSc-Bold.ttf"),
+    Roboto: require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>DogiPlay</Text>
+      <Text style={{ fontFamily: "DancingSc-Bold" }}>DogiPlay</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
