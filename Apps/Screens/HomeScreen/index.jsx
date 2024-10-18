5e6a309ab37704e3867ff996001e10c3214cea4f
CarlosAmezcua <Amezcua041196@gmail.com>
2024-10-18 10:52:07 -0700

    FIX: visualización de publicaciones

--- Apps/Screens/HomeScreen/index.jsx
+++ Apps/Screens/HomeScreen/index.jsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  Button,
-  TouchableOpacity,
-  StyleSheet,
-  FlatList,
-  Dimensions,
-} from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { View, StyleSheet, FlatList, Dimensions } from "react-native";
 import { useUser } from "@clerk/clerk-expo";
 import { supabase } from "../../Utils/SupabaseConfig";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import PostItem from "./PostItem";
-import { useRoute } from "@react-navigation/native";
+import { useFocusEffect, useRoute } from "@react-navigation/native";
+import Toast from "react-native-toast-message";
 
 export default function HomeScreen() {
   const { user } = useUser();
-  const params = useRoute().params;
   const [postList, setPostList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentMedia, setCurrentMedia] = useState(0);
@@ -27,31 +18,73 @@ export default function HomeScreen() {
 
   useEffect(() => {
     user && updateProfileImage();
+    setPage(0);
   }, [user]);
 
-  useEffect(() => {
-    getLatestPost();
-  }, []);
+  // useEffect(() => {
+  //   user && getLatestPost();
+  // }, [user]);
+
+  useFocusEffect(
+    useCallback(() => {
+      getLatestPost();
+    }, [])
+  );
 
   const getLatestPost = async () => {
-    const userSport = user?.sport;
     setLoading(true);
+
+    const userSport = await getUserSport();
+    console.log("🚀 ~ getLatestPost ~ userSport:", userSport);
+
+    if (!userSport) {
+      console.error("No se encontró deporte");
+      setLoading(false);
+      return;
+    }
+
     const { data, error } = await supabase
-      .from("Posts")
-      .select(
-        "*, Users(username, name, profileImage, sport), Reactions(postId, userEmail)"
-      )
-      .eq("Users.sport", userSport)
-      .range(page * 8, (page + 1) * 8 - 1) // Trae 8 publicaciones por página
+      .from("Users")
+      .select("name, username, email, sport, Posts (*)")
+      .eq(`Posts.emailRef`, user?.primaryEmailAddress.emailAddress)
+      .eq(`sport`, userSport)
+      .range(page * 8, (page + 1) * 8 - 1)
       .order("id", { ascending: false });
 
+    if (data == "" || data == []) {
+      Toast.show({
+        type: "error",
+        text1: "❌ Error de actualización",
+        text2: "Error al encontrar publicaciones.",
+      });
+    }
+
     if (!error) {
-      console.log("Fetched posts:", data);
-      setPostList((prevPostList) => [...prevPostList, ...data]);
-      setLoading(false);
+      console.log("Fetched posts:", data[0].Posts);
+      if (data[0]?.Posts) {
+        page === 0
+          ? setPostList(data[0].Posts)
+          : setPostList((prevPostList) => [...prevPostList, ...data[0].Posts]);
+      }
     } else {
       console.error("Error fetching posts from Supabase:", error);
     }
+
+    setLoading(false);
+  };
+
+  const getUserSport = async () => {
+    const { data, error } = await supabase
+      .from("Users")
+      .select("sport")
+      .eq("email", user?.primaryEmailAddress.emailAddress);
+
+    if (error) {
+      console.error("Error fetching user sport:", error);
+      return null;
+    }
+
+    return data?.[0]?.sport || null;
   };
 
   const loadMorePosts = () => {
