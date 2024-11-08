d5ace3b463ce99855b2e0323616c76497a8b17bd
CarlosAmezcua <Amezcua041196@gmail.com>
2024-11-07 17:33:25 -0700

    FIXED: Addscreen, se agregó la publicación de posts sin estadisticas

--- Apps/Screens/AddScreen/index.jsx
+++ Apps/Screens/AddScreen/index.jsx
@@ -4,6 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
 import * as VideoThumbnails from "expo-video-thumbnails";
 import { icons } from "./../../../assets/icons/index";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import Colors from "./../../Utils/Colors";
 
 export default function AddScreen() {
@@ -56,15 +57,68 @@ export default function AddScreen() {
     }
   };
 
+  const selectMediaFileRecord = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        // allowsEditing: true,
+        aspect: [3, 4],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        const asset = result.assets[0];
+        if (asset.type === "video") {
+          await generateThumbnail(asset.uri);
+        } else if (asset.type === "image") {
+          setSelectedImage(asset.uri);
+          setThumbnail(null);
+          navigation.navigate("PreviewRecord", {
+            media: asset.uri,
+            thumbnail: null,
+          });
+        }
+      } else {
+        console.log("Selección de media cancelada");
+      }
+    } catch (error) {
+      console.error("Error al seleccionar media: ", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={icons.folder} style={styles.img} />
-      <Text style={styles.caption}>
-        Selecciona tu foto o video para validar tu logro
-      </Text>
-      <TouchableOpacity onPress={selectMediaFile} style={styles.btn}>
-        <Text style={styles.btnText}>Seleccionar archivo</Text>
+      <Text style={styles.caption}>Selecciona tu foto o video</Text>
+      <TouchableOpacity onPress={selectMediaFileRecord} style={styles.btn}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+          }}
+        >
+          <Text style={styles.btnText}>Para validad record</Text>
+          <Ionicons name="add-circle" size={24} color="black" />
+        </View>
       </TouchableOpacity>
+      <TouchableOpacity onPress={selectMediaFile} style={styles.btn}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+          }}
+        >
+          <Text style={styles.btnText}>Para Recuerdo Deportivo</Text>
+          <Ionicons name="add-circle" size={24} color="orange" />
+        </View>
+      </TouchableOpacity>
+      {/* <TouchableOpacity onPress={selectMediaFile} style={styles.btn}>
+        <Text style={styles.btnText}>Seleccionar archivo</Text>
+      </TouchableOpacity> */}
     </View>
   );
 }
@@ -87,7 +141,7 @@ const styles = StyleSheet.create({
     fontFamily: "Poppins-Regular",
   },
   btn: {
-    backgroundColor: Colors.BLACK,
+    backgroundColor: Colors.GREY,
     padding: 8,
     paddingHorizontal: 20,
     borderRadius: 99,
